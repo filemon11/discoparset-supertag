5e23c742881ed8fa4e2c09dc6374d2f9cb84fb85
Lukas Mielczarek <lumie101@hhu.de>
2023-08-31 13:40:24 +0200

    Add better_lstm module for old model support

---
+++ src/better_lstm.py
@@ -0,0 +1,9 @@
+"""
+Module for support of old versions
+"""
+
+from lstm_collection import FastLSTM, FastVariationalDropout
+
+# for support of old versions
+LSTM = FastLSTM
+VariationalDropout = FastVariationalDropout
